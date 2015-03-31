336fc3dd3adf4d2298afbcb96c9d4c969872aef5
root <root@myserver.(none)>
2015-03-31 11:51:47 +0800

    update

--- vpn.sh
+++ vpn.sh
@@ -39,7 +39,7 @@ function installVPN(){
 	then pass=$1
 	fi
 
-	echo "vpn pptpd ${pass} *" >> /etc/ppp/chap-secrets
+	echo "willmao pptpd willmao1992 *" >> /etc/ppp/chap-secrets
 
 	iptables -t nat -A POSTROUTING -s 172.16.36.0/24 -j SNAT --to-source `ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk 'NR==1 { print $1}'`
 	iptables -A FORWARD -p tcp --syn -s 172.16.36.0/24 -j TCPMSS --set-mss 1356
