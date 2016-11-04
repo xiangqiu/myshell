28c01a4d4d8f4f1fe21e2ec96f3cf753981dfb0e
Will Mao <haijiao1945@163.com>
2016-11-04 14:15:36 +0800

    Update sync.py

--- sync.py
+++ sync.py
@@ -2,28 +2,37 @@
 import commands,os,re,sys
 
 gitdir = '/Users/maohuitao/Downloads/d1/'
-publishdir = '/Users/maohuitao/Downloads/d3/'
+git = '/Downloads/d1/'
+publish = '/Downloads/d3/'
 ignore = ['.DS_Store','log','conf','.git']
 
-def sync(gitdir,publishdir):
+os.system('cd '+gitdir+" && git pull")
+
+def fileCopy(tmp_x,tmp_old_x):
+    if os.path.exists(tmp_x):
+        x_time = os.path.getmtime(tmp_x)
+        old_x_time = os.path.getmtime(tmp_old_x)
+        if old_x_time > x_time:
+            os.system('cp -f '+tmp_old_x+' '+tmp_x)
+    else:
+        os.system('cp -f '+tmp_old_x+' '+tmp_x)
+    pass
+
+def sync(gitdir):
+    publishdir = gitdir.replace(git,publish)
+
     if os.path.exists(publishdir) == False:
         os.mkdir(publishdir)
     for x in os.listdir(gitdir):
         if x in ignore:
             continue
         if os.path.isdir(os.path.join(gitdir,x)):
-            publishdir = os.path.join(publishdir,x)
-            sync(os.path.join(gitdir,x),publishdir)
+            sync(os.path.join(gitdir,x))
         else:
             tmp_x = os.path.join(publishdir,x)
             tmp_old_x = os.path.join(gitdir,x)
-            if os.path.exists(tmp_x):
-                x_time = os.path.getmtime(tmp_x)
-                old_x_time = os.path.getmtime(tmp_old_x)
-                if old_x_time > x_time:
-                    os.system('cp -f '+tmp_old_x+' '+tmp_x)
-            else:
-                os.system('cp -f '+tmp_old_x+' '+tmp_x)
+            fileCopy(tmp_x,tmp_old_x)
         pass
 
-sync(gitdir,publishdir)
+sync(gitdir)
+
